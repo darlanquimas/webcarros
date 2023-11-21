f3b4fb420dc0d4de4673cc03e4c67f2aa592a0e2
Darlan Quimas <darlanquimas@gmail.com>
2023-11-20 21:56:52 -0300

    [infra] - configs router

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/layout";
+import Home from "../screens/public/home";
+import Detail from "../screens/private/detail";
+import Dashboard from "../screens/private/dashboard";
+import CreateNew from "../screens/private/createNew";
+import Login from "../screens/public/login";
+import Register from "../screens/public/register";
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/detail/:id", element: <Detail /> },
+      { path: "/detail/:id", element: <Dashboard /> },
+      { path: "/detail/:id", element: <CreateNew /> },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+]);
