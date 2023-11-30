974e8ba19f8e49499c5ce2661534397c58ef4946
Darlan Quimas <darlanquimas@gmail.com>
2023-11-30 20:05:05 -0300

    [feat] image upload

--- src/screens/private/dashboard/createNew/index.tsx
+++ src/screens/private/dashboard/createNew/index.tsx
@@ -1,9 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { ChangeEvent, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { FiUpload } from "react-icons/fi";
 import { z } from "zod";
 import Input from "../../../../components/input";
+import { AuthContext } from "../../../../contexts/AuthContext";
+import { v4 as uuidv4 } from "uuid";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../../../services/firebaseConnection";
 
 const schema = z.object({
   name: z.string().min(3, "O campo nome é obrigatório"),
@@ -24,6 +28,7 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 const CreateNew = () => {
+  const { user } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
@@ -38,10 +43,39 @@ const CreateNew = () => {
     console.log(data);
   }
 
+  async function handleFile(event: ChangeEvent<HTMLInputElement>) {
+    if (event.target.files && event.target.files[0]) {
+      const image = event.target.files[0];
+      if (image.type === "image/jpeg" || image.type === "image/png") {
+        await handleUpload(image);
+      } else {
+        alert("Envie uma imagem jpeg ou png");
+        return;
+      }
+    }
+  }
+
+  async function handleUpload(image: File) {
+    if (!user?.uid) {
+      return;
+    }
+
+    const currentUid = user?.uid;
+    const uidImage = uuidv4();
+
+    const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`);
+
+    await uploadBytes(uploadRef, image).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadUrl) => {
+        console.log(downloadUrl);
+      });
+    });
+  }
+
   return (
     <>
       <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
-        <button className="border-2 w-12 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-15 md:w-20 md:h-20 ">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-48 md:w-20 md:h-20 ">
           <div className="absolute cursor-pointer">
             <FiUpload size={30} color="#000" />
           </div>
@@ -50,6 +84,7 @@ const CreateNew = () => {
               type="file"
               accept="image/*"
               className="opacity-0 cursor-pointer  h-full"
+              onChange={handleFile}
             />
           </div>
         </button>
